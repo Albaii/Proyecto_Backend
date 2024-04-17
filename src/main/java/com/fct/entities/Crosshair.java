72b3e23cd1d5a244dfa9d80f523dd9f95f9f698b
Alby Delgado Morales (ext) <alby.delgado.external@eviden.com>
2024-04-17 09:32:39 +0100

    feat : Create the entities

---
+++ src/main/java/com/fct/entities/Crosshair.java
@@ -0,0 +1,31 @@
+package com.fct.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "crosshairs")
+public class Crosshair {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String crosshairCode;
+
+    @ManyToOne
+    @JoinColumn(name = "player_id")
+    private Player player;
+}
